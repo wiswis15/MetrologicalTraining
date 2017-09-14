be9d7ac8a415ea0ee285546f32783c0d03a6f8ee
Sander van der Maar <sandervandermaar@gmail.com>
2017-09-14 10:49:46 +0200

    Extends desktop build script with compositor+web UI

--- projects/WPEFramework/linux-desktop/scripts/build.sh
+++ projects/WPEFramework/linux-desktop/scripts/build.sh
@@ -2,18 +2,19 @@
 
 BASEDIR=$PWD
 
-rm -rf build.wpeframework/ build.plugins.open/ build.plugins.outofprocesstest/ prefix/
+rm -rf build.wpeframework/ build.plugins.open/ build.plugins.outofprocesstest/ build.plugins.compositor/ staging/
 mkdir -p staging/usr
 
 mkdir build.wpeframework
 cd build.wpeframework
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr -DINSTALL_HEADERS_TO_TARGET=ON -DWPEFRAMEWORK_TEST_APPS=ON -DWPEFRAMEWORK_TEST_LOADER=ON ../eclipse/src/
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr -DINSTALL_HEADERS_TO_TARGET=ON -DWPEFRAMEWORK_TEST_APPS=ON -DWPEFRAMEWORK_TEST_LOADER=ON ../eclipse/WPEFramework/src
 make install
 cd ..
 
 mkdir build.plugins.open
 cd build.plugins.open
 PKG_CONFIG_PATH=$BASEDIR/staging/usr/lib/pkgconfig/ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr \
+-DWPEFRAMEWORK_WEB_UI=ON \
 -DWPEFRAMEWORK_PLUGIN_COMMANDER=ON \
 -DWPEFRAMEWORK_PLUGIN_DEVICEINFO=ON \
 -DWPEFRAMEWORK_PLUGIN_LOCATIONSYNC=ON \
@@ -23,14 +24,18 @@ PKG_CONFIG_PATH=$BASEDIR/staging/usr/lib/pkgconfig/ cmake -DCMAKE_BUILD_TYPE=Deb
 -DWPEFRAMEWORK_PLUGIN_TRACECONTROL=ON \
 -DWPEFRAMEWORK_PLUGIN_WEBPROXY=ON \
 -DWPEFRAMEWORK_PLUGIN_WEBSERVER=ON \
--DWPEFRAMEWORK_PLUGIN_WEBSHELL=ON ../plugins/open/
+-DWPEFRAMEWORK_PLUGIN_WEBSHELL=ON ../eclipse/Plugins/Open
 make install
 cd ..
 
 mkdir build.plugins.outofprocesstest
 cd build.plugins.outofprocesstest
-PKG_CONFIG_PATH=$BASEDIR/staging/usr/lib/pkgconfig/ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr ../plugins/OutOfProcessTest/
+PKG_CONFIG_PATH=$BASEDIR/staging/usr/lib/pkgconfig/ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr ../eclipse/Plugins/OutOfProcessTest/
 make install
 cd ..
 
-
+mkdir build.plugins.compositor
+cd build.plugins.compositor
+PKG_CONFIG_PATH=$BASEDIR/staging/usr/lib/pkgconfig/ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$BASEDIR/staging/usr -DWPEFRAMEWORK_PLUGIN_COMPOSITOR_IMPLEMENTATION=Stub -DWPEFRAMEWORK_PLUGIN_COMPOSITOR_SERVER=ON ../eclipse/Plugins/Compositor/
+make install
+cd ..
