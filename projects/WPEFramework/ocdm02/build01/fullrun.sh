f7b5f1a202c1b68ba30b3dd4d97953e0eb2bab06
Sander van der Maar <sandervandermaar@gmail.com>
2018-08-28 15:12:16 +0200

    Fix fullrun.sh script

--- projects/WPEFramework/ocdm02/build01/fullrun.sh
+++ projects/WPEFramework/ocdm02/build01/fullrun.sh
@@ -5,18 +5,18 @@ rm -rf build staging
 ./wpeframework-copy.sh
 ./wpeframework-build.sh
 
+./wpeframework-ocdm-copy.sh
+./wpeframework-ocdm-build.sh
+
 ./wpeframework-plugins-copy.sh
 ./wpeframework-plugins-patch.sh
 ./wpeframework-plugins-build.sh
 
-./wpeframework-ocdm-copy.sh
-./wpeframework-ocdm-build.sh
-
 ./ocdm-null2-copy.sh
 ./ocdm-null2-build.sh
 
-./ocdmtestapp-copy.sh
-./ocdmtestapp-build.sh
+./ocdmtestapp01-copy.sh
+./ocdmtestapp01-build.sh
 
 python/updateConfig.py
 
