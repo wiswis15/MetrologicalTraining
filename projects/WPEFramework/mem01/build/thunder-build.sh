136dfc5c2decf6d15a4a531caa8299f28fa6cb70
Sander van der Maar <sandervandermaar@gmail.com>
2020-01-09 11:24:13 +0100

    Minor cleanup

--- projects/WPEFramework/mem01/build/thunder-build.sh
+++ projects/WPEFramework/mem01/build/thunder-build.sh
@@ -11,7 +11,9 @@ SRC_DIR="$PWD/repos/Thunder"
 rm -rf build/thunder/build
 mkdir -p build/thunder/build
 cd build/thunder/build
+
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$STAGING_DIR -DCMAKE_CXX_FLAGS='-O0 -g -m32' -DCMAKE_C_FLAGS='-O0 -g -m32' -DOOMADJUST="" -DSYSTEM_PREFIX="NOS" -DVIRTUALINPUT=ON -DEXTERN_EVENTS='WebSource' -DCMAKE_STRIP='/usr/bin/echo' -DMEMORYOVERVIEW=ON $SRC_DIR
+
 make -j4
 make install
-cd ..
+
