fbca857b744f5815f4d5b3f6bc164189b9e5d9c1
Sander van der Maar <sandervandermaar@gmail.com>
2020-04-29 11:22:49 +0200

    clean up

--- projects/WPEFramework/tools01/apps/03-cipher/src/main.cpp
+++ projects/WPEFramework/tools01/apps/03-cipher/src/main.cpp
@@ -1,136 +1,104 @@
 #include <core/core.h>
-#include <openssl/bio.h>
-#include <openssl/evp.h>
-#include <openssl/aes.h>
+#include <com/com.h>
 #include <iostream>
-#include <fstream>
-#include <openssl/modes.h>
+#include <inttypes.h>
+#include <cryptography/cryptography.h>
+#include <plugins/plugins.h>
 
 using namespace std;
+using namespace WPEFramework;
 
-// TODO: is the ESN always the same length?
-const uint32_t g_esnSize = 42;
-
-const uint8_t g_key[16] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 
-                            0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x11 };
-
-// TODO: m_ prefix
-struct NetflixData
+int main()
 {
-    uint8_t salt[16];
-    uint8_t kpe[16];
-    uint8_t kph[32];
-    uint8_t esn[g_esnSize];
-} __attribute__((packed));
 
-// TODO: check buffer overrun
-void DecodeBase64(const char input[], uint8_t output[], uint32_t outputSize)
-{
-	BIO *bio, *b64;
 
-	int inputLength = strlen(input);
+    //Cryptography::ICryptography* cryptography = Cryptography::ICryptography::Instance("/tmp/communicator");
 
-	b64 = BIO_new(BIO_f_base64());
-	bio = BIO_new_mem_buf(input, inputLength);
-	bio = BIO_push(b64, bio);
-	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL);
-	BIO_read(bio, output, inputLength);
+//    Cryptography::INetflixSecurity * netflixSec = vault->QueryInterface<Cryptography::INetflixSecurity>();
+//    cerr << netflixSec << endl;
 
-	BIO_free_all(bio);
+    WPEFramework::Cryptography::INetflixSecurity* nfSecurity = WPEFramework::Cryptography::INetflixSecurity::Instance();
+    cout << "nfSecurity: " << nfSecurity << endl;
+
+    Cryptography::ICryptography* cryptography = Cryptography::ICryptography::Instance("");
+    cerr << cryptography << endl;
+
+    Cryptography::IVault * vault = cryptography->Vault(CRYPTOGRAPHY_VAULT_NETFLIX);
+    cerr << "vault = " << vault << endl;
+
+    //nfSecurity
+
+    cout << "ESN = >>" << nfSecurity->ESN() << "<<" << endl;
+
+
+/*
+    Core::NodeId nodeId("/tmp/communicator");
+
+    Core::ProxyType< RPC::InvokeServerType<1, 0, 4> > engine(
+        Core::ProxyType<RPC::InvokeServerType<1, 0, 4>>::Create());
+
+    Core::ProxyType<RPC::CommunicatorClient> client(
+        Core::ProxyType<RPC::CommunicatorClient>::Create(
+            nodeId,
+            Core::ProxyType< Core::IIPCServer >(engine)));
+
+    engine->Announcements(client->Announcement());
+
+    //PluginHost::IShell* controller = client->Open<PluginHost::IShell>(_T("Dictionary"), ~0, 3000);
+    PluginHost::IPlugin* controller = client->Open<PluginHost::IPlugin>(_T("Controller"), ~0, 3000);
+    //Cryptography::INetflixSecurity* ns = client->Open<Cryptography::INetflixSecurity>(_T("NetflixSecurity"), ~0, 3000);
+    //Cryptography::ICryptography* c01 = client->Open<Cryptography::ICryptography>(_T("CryptographyImpl"), ~0, 3000);
+
+    //cerr << "ns: " << ns << endl;
+    //cerr << "c01: " << c01 << endl;
+    cerr << "controller: " << controller << endl;
+*/
+
+/*
+    const string connectorName = "/tmp/communicator";
+    Core::NodeId remoteNode(connectorName.c_str());
+
+    //Core::ProxyType<WorkerPoolImplementation> _engine;
+    Core::ProxyType<Core::WorkerPool> _engine;
+    uint8_t threadCount = 4;
+    //_engine = Core::ProxyType<WorkerPoolImplementation>::Create(threadCount, Core::Thread::DefaultStackSize(), 16);
+    _engine = Core::ProxyType<Core::WorkerPool>::Create(threadCount, Core::Thread::DefaultStackSize(), 16);
+
+    Core::IWorkerPool::Assign(&(*_engine));
+
+    Core::ProxyType<RPC::CommunicatorClient> _server;
+    _server = (Core::ProxyType<RPC::CommunicatorClient>::Create(remoteNode, Core::ProxyType<Core::IIPCServer>(_engine)));
+    //_engine->Announcements(_server->Announcement());
+
+    Exchange::IAdder * adder = client->Open<Exchange::IAdder>(_T("Adder"));
+
+    cout << 
+*/
+
+
+/*
+    //Core::ProxyType<RPC::InvokeServerType<4, 1, 8>> engine(Core::ProxyType<RPC::InvokeServerType<4, 1, 8>>::Create());
+
+    Core::ProxyType<Core::IIPCServer> engine(Core::ProxyType<RPC::InvokeServer>::Create(&Core::WorkerPool::Instance()));
+    
+    Core::ProxyType<RPC::CommunicatorClient>::Create(remoteNode, engine);
+    //Core::ProxyType<RPC::CommunicatorClient> client(Core::ProxyType<RPC::CommunicatorClient>::Create(engine));
+    */
+
+/*
+    Core::ProxyType<RPC::CommunicatorClient> client(
+        Core::ProxyType<RPC::CommunicatorClient>::Create(
+            remoteNode,
+            Core::ProxyType<Core::IIPCServer>(engine)
+        ));
+*/
+
+
+/*
+    engine->Announcements(client->Announcement());
+*/
+
+    // Exchange::IAdder * adder = client->Open<Exchange::IAdder>(_T("Adder"));
+
+    Core::Singleton::Dispose();
 }
-
-void runAes(const uint8_t input[], uint8_t output[], uint32_t bufferSize, const uint8_t iv[16])
-{
-   unsigned char localIv[AES_BLOCK_SIZE];
-   memcpy(localIv, iv, 16);
-
-   unsigned char ecount[AES_BLOCK_SIZE];
-   memset(ecount, 0, 16); 
-
-   unsigned int num = 0;
-
-   AES_KEY aesKey;
-   AES_set_encrypt_key(g_key, 128, &aesKey);
-   CRYPTO_ctr128_encrypt(input, output, bufferSize, &aesKey, localIv, ecount, &num, reinterpret_cast<block128_f>(AES_encrypt));
-}
-
-// TODO: argv as array
-// TODO: better error return codes (standard linux)
-int main(int argc, const char ** argv)
-{
-    if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " [input file] [output file]" << endl;
-        cerr << "Example:" << endl;
-        cerr << "   " << argv[0] << " idfile netflix-vault.bin" << endl;
-        return 1;
-    }
-
-    NetflixData netflixData;
-
-    // 1. Create random IV vector and salt.
-    srand(time(nullptr));
-    uint8_t iv[16];
-    for (int i = 0; i < 16; i++) {
-        iv[i] = rand() % 256;
-        netflixData.salt[i] = rand() % 256;
-    }
-
-    ifstream inFile(argv[1]);
-    if (!inFile) {
-        cerr << "Failed to open " << argv[1] << " for reading" << endl;
-        return 2;
-    }
-
-    string line;
-
-    // 2. Read ESN and copy into NetflixData struct.
-    std::getline(inFile, line);
-    if (line.length() != g_esnSize) {
-        cerr << "Expected ESN to be " << g_esnSize << " chars long, got " << line.length() << " instead." << endl;
-        return 3;
-    }
-
-    strncpy(reinterpret_cast<char *>(netflixData.esn), line.c_str(), g_esnSize);
-
-    // 3. Read KPE (base 64 encoded) and store in NetflixData struct.
-    const uint32_t expectedKpeB64Length = 24;
-    std::getline(inFile, line);
-    if (line.length() != expectedKpeB64Length) {
-        cerr << "Expected KPE to be " << expectedKpeB64Length << " chars long, got " << line.length() << " instead." << endl;
-        return 4;
-    }
-
-    DecodeBase64(line.c_str(), netflixData.kpe, sizeof(netflixData.kpe));
-
-    // 3. Read KPH (base 64 encoded) and store in NetflixData struct.
-    const uint32_t expectedKphB64Length = 44;
-    std::getline(inFile, line);
-    if (line.length() != expectedKphB64Length) {
-        cerr << "Expected KPH to be " << expectedKphB64Length << " chars long, got " << line.length() << " instead." << endl;
-        return 5;
-    }
-
-    DecodeBase64(line.c_str(), netflixData.kph, sizeof(netflixData.kph));
-
-    // 4. Encrypt NetflixData struct 
-    uint8_t encryptBuffer[sizeof(NetflixData)];
-    memset(encryptBuffer, 0, sizeof(encryptBuffer));
-    runAes(reinterpret_cast<uint8_t *>(&netflixData), encryptBuffer, sizeof(NetflixData), iv);
-
-    ofstream outFile(argv[2], ofstream::binary);
-    if (!outFile) {
-        cerr << "Failed to open " << argv[2] << " for writing" << endl;
-        return 6;
-    }
-
-    // 5. Write IV followed by encrypted NetflixData.
-    outFile.write(reinterpret_cast<const char *>(iv), sizeof(iv));
-    outFile.write(reinterpret_cast<const char *>(&encryptBuffer), sizeof(netflixData));
-
-    outFile.close();
-    inFile.close();
-
-    cout << "SUCCESS: Written netflix fault to " << argv[2] << endl;
-    return 0;
-}
-
